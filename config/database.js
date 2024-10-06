6f4283e7751b524a4830cbf35c99e76d2fc70ccf
Fernando <f.henrique.fraga@gmail.com>
2024-10-06 19:46:17 -0300

    Arquivo com a configuração para a conexão com o banco postgresql

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg'); 
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',  
+    port: process.env.DB_PORT,
+});
+
+module.exports = { sequelize, pool };
